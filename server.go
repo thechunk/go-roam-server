c7b3bfa45d086a4a7cc62cd6ee3c1f7adee46647
Russell <thechunk@gmail.com>
2017-11-16 22:50:50 +0800

    Add Redis cxn

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"os"
 	"github.com/gin-gonic/gin"
+	"github.com/garyburd/redigo/redis"
 )
 
 func main() {
@@ -10,6 +12,19 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+
+	redisHost := os.Getenv("REDIS_URL")
+	if redisHost == "" {
+		redisHost = ":6379"
+	}
+	c, err := redis.Dial("tcp", redisHost)
+	_ = c
+
+	r.GET("/redis", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"error": err,
 		})
 	})
 
