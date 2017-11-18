c9ed672b26c7c5bcd1a72750aefb1759f1d0c97d
Russell Cheung <thechunk@gmail.com>
2017-11-18 16:25:58 +0800

    Start unit tests on entry point

---
+++ server_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"testing"
+)
+
+type Engine struct {
+}
+
+func GET(relativePath string, cb ...gin.HandlerFunc) gin.IRoutes {
+	return gin.IRoutes{}
+}
+func Run(s ...string) error {
+}
+
+func TestMain(t *testing.T) {
+}
